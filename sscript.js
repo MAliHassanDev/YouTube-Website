ceaa94f7985e02644ed0b96b05ec98bbeed46a14
Muhammad Ali Hassan <muhammadalihassanofficial@gmail.com>
2024-01-08 20:36:22 +0500

    changed top padding from main container to body

--- sscript.js
+++ sscript.js
@@ -3,10 +3,9 @@ var sidebar = document.querySelector(".sidebar");
 var tabicons = document.querySelector(".topic-tabs-container");
 var maincontainer = document.querySelector(".main-container-section");
 
-
-
 menuIcon.onclick = function(){
   sidebar.classList.toggle("small-sidebar");
   tabicons.classList.toggle("tab-links");
   maincontainer.classList.toggle("mainsection-toggle");
 }
+
