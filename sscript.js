5bbe1b15b55f91f569058e8003eacccae413fbc4
Muhammad Ali Hassan <muhammadalihassanofficial@gmail.com>
2024-01-09 17:57:23 +0500

    added a footer

--- sscript.js
+++ sscript.js
@@ -9,3 +9,23 @@ menuIcon.onclick = function(){
   maincontainer.classList.toggle("mainsection-toggle");
 }
 
+
+// Enable hidden nav bar
+
+const header = document.querySelector(".header");
+const topictabs = document.querySelector(".topic-tabs-container");
+let lastScrollY = window.scrollY;
+
+window.addEventListener("scroll", () => {
+ if (window.scrollY>lastScrollY) {
+     header.classList.add("header-hidden")
+     topictabs.classList.add("topictabs-hidden");
+  } else {
+    header.classList.remove("header-hidden")
+    topictabs.classList.remove("topictabs-hidden");
+  }
+
+  lastScrollY = window.scrollY;
+});
+
+
